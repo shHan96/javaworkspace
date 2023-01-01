452be1297b058d7dd268f2b3e4e5253af3ae6860
Hansang <f125678@naver.com>
2023-01-01 16:47:03 +0900

    챕터1 퀴즈

---
+++ src/chapter01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNumber = "상암08"; // 버스번호
+        int restTime = 3; // 남은 시간
+        double restDistance = 1.2; // 남은 거리
+
+        System.out.println(busNumber+" 버스");
+        System.out.println("약 "+restTime + "분 후 도착");
+        System.out.println("남은 거리 "+restDistance+"km");
+    }
+}
